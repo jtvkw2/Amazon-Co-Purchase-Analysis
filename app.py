7a26dde5405650ea61836597ac426b37c6b76e2d
jtvkw2 <jtvkw2@gmail.com>
2021-12-14 22:02:46 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import sys
 import time
-from xml.etree.ElementTree import TreeBuilder
 import pandas as pd
 import numpy as np
 import ProccesedData as prd
@@ -12,7 +11,7 @@ from scipy.stats import pearsonr
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QStandardItem, QStandardItemModel, QFont
-from PyQt5 import uic, QtGui
+from PyQt5 import uic
 
 
 qtCreatorFile = "app.ui"
@@ -23,12 +22,15 @@ class AmazonApp(QMainWindow):
         super(AmazonApp, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.reccDF = None
-        self.userDF = None
 
         self.driver = GraphDatabase.driver('neo4j://40.77.108.181:7687', auth=('neo4j', 'amazondb'))
         self.wordList = prd.get_wordlist()
         self.current_search = ['1', '48724']
+        self.groupFilt = None
+        self.itemRatingFilt = None
+        self.itemReviewsFilt = None
+        self.userRatingFilt = None
+        self.userReviewsFilt = None
         self.itemsSortBy = None
 
         completer = QCompleter(self.wordList, self)
@@ -36,24 +38,11 @@ class AmazonApp(QMainWindow):
         self.ui.inputLine.setCompleter(completer)
         self.ui.errorLabel.setVisible(False)
         self.ui.addButton.clicked.connect(self.addButtonPressed)
-        self.ui.recButton.clicked.connect(partial(self.RecButtonPressed,'item'))
+        self.ui.itemRecButton.clicked.connect(partial(self.RecButtonPressed,'item'))
         self.ui.userRecButton.clicked.connect(partial(self.RecButtonPressed,'user'))
         self.ui.algList.itemSelectionChanged.connect(self.algoSelected)
         self.ui.itemClearButton.clicked.connect(self.itemClearPressed)
         self.ui.clearAllButton.clicked.connect(self.clearAllPressed)
-        self.ui.recAscend.clicked.connect(partial(self.sort_df, 'itemA'))
-        self.ui.userAscend.clicked.connect(partial(self.sort_df, 'userA'))
-        self.ui.recDecend.clicked.connect(partial(self.sort_df, 'itemD'))
-        self.ui.userDecend.clicked.connect(partial(self.sort_df, 'userD'))
-        self.ui.recSort1.itemSelectionChanged.connect(partial(self.sort_df, 'item'))
-        self.ui.recSort2.itemSelectionChanged.connect(partial(self.sort_df, 'item'))
-        self.ui.recSort3.itemSelectionChanged.connect(partial(self.sort_df, 'item'))
-        self.ui.recSort4.itemSelectionChanged.connect(partial(self.sort_df, 'item'))
-        self.ui.userSort1.itemSelectionChanged.connect(partial(self.sort_df, 'user'))
-        self.ui.userSort2.itemSelectionChanged.connect(partial(self.sort_df, 'user'))
-        self.ui.userSort3.itemSelectionChanged.connect(partial(self.sort_df, 'user'))
-        self.ui.userSort4.itemSelectionChanged.connect(partial(self.sort_df, 'user'))
-
 
     def close(self):
         # Don't forget to close the driver connection when you are finished with it
@@ -71,17 +60,16 @@ class AmazonApp(QMainWindow):
     def RecButtonPressed(self, type):
         #check: ' if filter == "select filter": then filter = None '
         # (because of how the filter list properties are set up on the UI
-        # if type == 'item':
-        #     self.groupFilt = self.ui.groupList.currentText()
-        #     self.itemRatingFilt = self.ui.minItemAvgRating.currentText()
-        #     self.itemReviewsFilt = self.ui.itemMinReviews.text()
-        #     self.itemsSortBy = self.ui.itemsSortBy #idk if you want this as a class property
-        # elif type == 'user':
-        #     #check: ' if filter == "select filter": then filter = None '
-        #     # (because of how the filter list properties are set up on the UI
-        #     self.userRatingFilt = self.ui.minUserAvgRating.currentText()
-        #     self.userReviewsFilt = self.ui.userMinReviews.text()
-        #     self.userSortBy = self.ui.userSortBy #idk if you want this as a class property
+        if type == 'item':
+            self.groupFilt = self.ui.groupList.currentText()
+            self.itemRatingFilt = self.ui.minItemAvgRating.currentText()
+            self.itemsSortBy = self.ui.itemsSortBy #idk if you want this as a class property
+        elif type == 'user':
+            #check: ' if filter == "select filter": then filter = None '
+            # (because of how the filter list properties are set up on the UI
+            self.userRatingFilt = self.ui.minUserAvgRating.currentText()
+            self.userReviewsFilt = self.ui.userMinReviews.text()
+            self.userSortBy = self.ui.userSortBy #idk if you want this as a class property
         if len(self.current_search) == 0:
             self.ui.errorLabel.setText('Please Select Items')
             self.ui.errorLabel.setVisible(True)
@@ -107,8 +95,9 @@ class AmazonApp(QMainWindow):
                     temp_nodes = recDB.write_transaction(self._search, qg.dfs_query(int(self.current_search[i])))
                     if temp_nodes:
                         temp_list = self.recordToList(temp_nodes, 'ids', type)
-                        inter = curr_nodes.intersection(temp_list)
-                        all_nodes_id = list(inter)
+                        for n in temp_list:
+                            curr_nodes.add(n)
+                        all_nodes_id = list(curr_nodes)
 
                 final_data = []
                 for id in all_nodes_id:
@@ -138,20 +127,11 @@ class AmazonApp(QMainWindow):
                                     final_data.append(curr_data)
                 if type == 'item':
                     final_df = pd.DataFrame(final_data, columns = ['id', 'title', 'type', 'rating', 'salesrank', 'similarity'])
-                    final_df.sort_values('similarity')
-                    self.reccDF = final_df
                     self.outputToUI(final_df, type)
                 if type == 'user':
                     final_df = pd.DataFrame(final_data, columns = ['id', 'rating', 'reviews', 'similarity'])
-                    final_df.sort_values('similarity')
-                    self.userDF = final_df 
                     self.outputToUI(final_df, type)
-                ttlTime = time.time() - start_time
-                print("--- %s seconds ---" % (ttlTime))
-                if type == 'item':
-                    self.ui.recTime.setPlainText(str(round(ttlTime, 4)))
-                if type == 'user':
-                    self.ui.userTime.setPlainText(str(round(ttlTime, 4)))
+                print("--- %s seconds ---" % (time.time() - start_time))
 
 
     def recordToList(self, data, input, type):
@@ -176,12 +156,12 @@ class AmazonApp(QMainWindow):
         list1 = np.asarray(list1, dtype='float64')
         list2 = np.asarray(list2, dtype='float64')
 
-        if algorithm == 'cosine': #Cosine Sim
+        if algorithm == 'algorithm1': #Cosin Sim
             return 1 - distance.cosine(list1, list2)
-        elif algorithm == 'Pearson': # Pearson
-            return pearsonr(list1, list2)
-        elif algorithm == 'Jaccard': # Jaccard
-            return distance.jaccard(list1, list2)
+        elif algorithm == 'algorithm2': # Pearson
+            return pearsonr(list1, list2)[0]
+        elif algorithm == 'algorithm3': # Jaccard
+            return sdistance.jaccard(list1, list2)
         else:
             return 0
 
@@ -199,201 +179,56 @@ class AmazonApp(QMainWindow):
     def clearAllPressed(self):
         self.ui.inputLine.clear()
         self.ui.inputList.clear()
-        for i in reversed(range(self.ui.recTable.rowCount())):
-            self.ui.recTable.removeRow(i)
+        self.ui.itemRecList.clear()
         current_search = []
 
     def outputToUI(self, df, type):
         output_df = df[df.similarity != 1.0]
-        style = "::section {""background-color: #f3f3f3; }"
         if type == 'item':
-            results = output_df.to_numpy()
-            self.ui.recTable.horizontalHeader().setStyleSheet(style)
-            self.ui.recTable.setColumnCount(len(results[0]))
-            self.ui.recTable.setRowCount(len(results))
-            self.ui.recTable.setHorizontalHeaderLabels(['id', 'title', 'type', 'rating', 'salesrank', 'similarity'])
-            self.ui.recTable.resizeColumnsToContents()
-            self.ui.recTable.setColumnWidth(0, 80)
-            self.ui.recTable.setColumnWidth(1, 220)
-            self.ui.recTable.setColumnWidth(2, 80)
-            self.ui.recTable.setColumnWidth(3, 80)
-            self.ui.recTable.setColumnWidth(4, 80)
-            self.ui.recTable.setColumnWidth(5, 160)
-            rowCount = 0
-            colCount = 0
-            for row in results:
-                for colCount in range(len(results[0])):
-                    temp = str(row[colCount])
-                    self.ui.recTable.setItem(rowCount, colCount % 6, QTableWidgetItem(temp))
-                    colCount += 1
-                rowCount += 1
-            # if self.groupFilt is not None and self.groupFilt !='Select group':
-            #     output_df = output_df[self.groupFilt]
-            # if self.itemRatingFilt is not None and self.itemRatingFilt != 'Select minimum rating':
-            #     output_df = output_df[output_df['rating'] >= self.itemRatingFilt]
-            # output_df = self.sort_df(output_df, type)
-            # for index, row in output_df.iterrows():
-            #     out_str = str(row['title']) + " \n    Similarity: " + str(row['similarity'])
-            #     self.ui.outputList.append(out_str)
+            if self.groupFilt is not None and self.groupFilt !='Select group':
+                output_df = output_df[self.groupFilt]
+            if self.itemRatingFilt is not None and self.itemRatingFilt != 'Select minimum rating':
+                output_df = output_df[output_df['rating'] >= self.itemRatingFilt]
+            output_df = self.sort_df(output_df, type)
+            for index, row in output_df.iterrows():
+                out_str = str(row['title']) + " \n    Similarity: " + str(row['similarity'])
+                self.ui.outputList.append(out_str)
         elif type == 'user':
-            results = output_df.to_numpy()
-            self.ui.userTable.horizontalHeader().setStyleSheet(style)
-            self.ui.userTable.setColumnCount(len(results[0]))
-            self.ui.userTable.setRowCount(len(results))
-            self.ui.userTable.setHorizontalHeaderLabels(['id', 'rating', 'reviews', 'similarity'])
-            self.ui.userTable.resizeColumnsToContents()
-            self.ui.userTable.setColumnWidth(0, 40)
-            self.ui.userTable.setColumnWidth(1, 110)
-            self.ui.userTable.setColumnWidth(2, 50)
-            self.ui.userTable.setColumnWidth(3, 40)
-            rowCount = 0
-            colCount = 0
-            for row in results:
-                for colCount in range(len(results[0])):
-                    temp = str(row[colCount])
-                    self.ui.userTable.setItem(rowCount, colCount, QTableWidgetItem(temp))
-                rowCount += 1
-            # output_df = self.sort_df(output_df, type)
-            # if self.userRatingFilt is not None and self.userRatingFilt != 'Select minimum rating':
-            #     output_df = output_df[output_df['rating'] >= self.userRatingFilt]
-            # if self.userReviewsFilt is not None:
-            #     output_df = output_df[output_df['reviews'] >= self.userReviewsFilt]
-            # for index, row in output_df.iterrows():
-            #     out_str = str(row['id']) + " \n    Similarity: " + str(row['similarity'])
-            #     self.ui.outputList.append(out_str)
+            output_df = self.sort_df(output_df, type)
+            if self.userRatingFilt is not None and self.userRatingFilt != 'Select minimum rating':
+                output_df = output_df[output_df['rating'] >= self.userRatingFilt]
+            if self.userReviewsFilt is not None:
+                output_df = output_df[output_df['reviews'] >= self.userReviewsFilt]
+            for index, row in output_df.iterrows():
+                out_str = str(row['id']) + " \n    Similarity: " + str(row['similarity'] + "\n")
+                self.ui.outputList.append(out_str)
 
-    def fillResults(self, df):
-        output_df = df[df.similarity != 1.0]
-        style = "::section {""background-color: #f3f3f3; }"        
-        self.ui.recTable.openPersistentEditor()
-        self.ui.recTable.horizontalHeader().setStyleSheet(style)
-        self.ui.recTable.setColumnCount(len(results[0]))
-        self.ui.recTable.setRowCount(len(results))
-        self.ui.recTable.setHorizontalHeaderLabels(['id', 'title', 'type', 'rating', 'salesrank', 'similarity'])
-        self.ui.recTable.resizeColumnsToContents()
-        self.ui.recTable.setColumnWidth(0, 80)
-        self.ui.recTable.setColumnWidth(1, 220)
-        self.ui.recTable.setColumnWidth(2, 80)
-        self.ui.recTable.setColumnWidth(3, 80)
-        self.ui.recTable.setColumnWidth(4, 80)
-        self.ui.recTable.setColumnWidth(5, 160)
-        rowCount = 0
-        colCount = 0
-        for row in results:
-            for colCount in range(len(results[0])):
-                temp = str(row[colCount])
-                self.ui.recTable.setItem(rowCount, colCount, QTableWidgetItem(temp))
-            rowCount += 1
-
-    def fillUserResults(self, df):
-        output_df = df[df.similarity != 1.0]
-        style = "::section {""background-color: #f3f3f3; }"        
-        results = output_df.to_numpy()
-        self.ui.recTable.horizontalHeader().setStyleSheet(style)
-        self.ui.recTable.setColumnCount(len(results[0]))
-        self.ui.recTable.setRowCount(len(results))
-        self.ui.recTable.setHorizontalHeaderLabels(['id', 'rating', 'reviews', 'similarity'])
-        self.ui.recTable.resizeColumnsToContents()
-        self.ui.recTable.setColumnWidth(0, 120)
-        self.ui.recTable.setColumnWidth(1, 260)
-        self.ui.recTable.setColumnWidth(2, 120)
-        self.ui.recTable.setColumnWidth(5, 180)
-        rowCount = 0
-        colCount = 0
-        for row in results:
-            for colCount in range(len(results[0])):
-                temp = str(row[colCount])
-                self.ui.userTable.setItem(rowCount, colCount, QTableWidgetItem(temp))
-            rowCount += 1
-
-
-    def sort_df(self, typeSend):
-        type = None
-        updwn = None
-        if typeSend == 'itemA':
-            type = 'item'
-            updwn = True
-        if typeSend == 'userA':
-            type = 'user'
-            updwn = True
-        if typeSend == 'itemD':
-            type = 'item'
-            updwn = False
-        if typeSend == 'userD':
-            type = 'user'
-            updwn = False
+    def sort_df(self, df, type):
         if type == 'item':
-            sort1, sort2, sort3, sort4 = None, None, None, None
-            rs1 = self.ui.recSort1.selectedItems()
-            if rs1:
-                sort1 = rs1[0].text()
-            rs2 = self.ui.recSort2.selectedItems()
-            if rs2:
-                sort2 = rs2[0].text()
-            rs3 = self.ui.recSort3.selectedItems()
-            if rs3:
-                sort3 = rs3[0].text()
-            rs4 = self.ui.recSort4.selectedItems()
-            if rs4:
-                sort4 = rs4[0].text()
-            sortList = []
-            if sort1:
-                sortList.append(sort1)
-            if sort2:
-                sortList.append(sort2)
-            if sort3:
-                sortList.append(sort3)
-            if sort4:
-                sortList.append(sort4)
-            if len(sortList) == 1:
-                if sortList[0] == 'similarity':
-                    return
-            if len(sortList) > 0:
-                setList = set(sortList)
-                sortString = "' , '".join(map(str, setList))
-                sstr = "'similarity' , '" + sortString + "'"
-                new_df = self.reccDF
-                new_df.sort_values(by= [sstr], ascending = updwn, inplace = True)
-            self.fillResults(self, new_df)
-        if type == 'user':
-            sort1, sort2, sort3, sort4 = None, None, None, None
-            us1 = self.ui.userSort1.selectedItems()
-            if us1:
-                sort1 = us1[0].text()
-            us2 = self.ui.userSort2.selectedItems()
-            if us2:
-                sort2 = us2[0].text()
-            us3 = self.ui.userSort3.selectedItems()
-            if us3:
-                sort3 = us3[0].text()
-            us4 = self.ui.userSort4.selectedItems()
-            if us4:
-                sort4 = us4[0].text()
-            sortList = []
-            if sort1:
-                sortList.append(sort1)
-            if sort2:
-                sortList.append(sort2)
-            if sort3:
-                sortList.append(sort3)
-            if sort4:
-                sortList.append(sort4)
-            if len(sortList) == 1:
-                if sortList[0] == 'similarity':
-                    return
-            table_row = []
-            for i in reversed(range(self.ui.recTable.rowCount())):
-                row = self.ui.recTable.row(i)
-                for item in row:
-                    table_row.append(str(item))
-                self.ui.recTable.removeRow(i)
-            old_df = pd.DataFrame(old_table)
-            old_df.columns('id', 'rating', 'reviews', 'similarity')
-            if len(sortList) > 0:
-                sortString = "' , '".join(map(str, sortList))
-                sstr = "'" + sortString + "'"
-                old_df.sort_values(by= [sstr], ascending = False, inplace = True)
-            self.outputToUI(self, old_df, type)
+            if self.itemsSortBy == 'Alphabetically A-Z':
+                return df.sort_values(by = ['title'])
+            elif self.itemsSortBy == 'Alphabetically Z-A':
+                return df.sort_values(by=['title'], ascending = False)
+            elif self.itemsSortBy == 'Average Rating':
+                return df.sort_values(by = ['rating'])
+            elif self.itemsSortBy == 'Salesrank':
+                return df.sort_values(by = ['salesrank'])
+            else:
+                return df.sort_values(by = ['similarity'])
+        elif type == 'user':
+            if self.userSortBy == 'Alphabetically A-Z':
+                return df.sort_values(by = ['id'])
+            elif self.userSortBy == 'Alphabetically Z-A':
+                return df.sort_values(by=['id'], ascending = False)
+            elif self.userSortBy == 'Average Rating':
+                return df.sort_values(by = ['rating'])
+            elif self.userSortBy == 'Number of Reviews':
+                return df.sort_values(by = ['rating'])
+            else:
+                return df.sort_values(by = ['similarity'], ascending = False)
+        else:
+            return df
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
